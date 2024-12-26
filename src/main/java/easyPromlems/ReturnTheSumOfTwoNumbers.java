652e47c35c3ae3b9e2bb20676e3a9d1c7b91dd8c
Aidai <aidaiurkalieva7@gmail.com>
2024-12-26 19:12:52 +0600

    First commit

---
+++ src/main/java/easyPromlems/ReturnTheSumOfTwoNumbers.java
@@ -0,0 +1,7 @@
+package easyPromlems;
+
+public class ReturnTheSumOfTwoNumbers {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/easyProblems/ReturnTheSumOfTwoNumberTest.java
@@ -0,0 +1,13 @@
+package easyProblems;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ReturnTheSumOfTwoNumberTest {
+    @Test
+    public void testAddPositiveNumbers() {
+        ReturnTheSumOfTwoNumberTest calculator = new ReturnTheSumOfTwoNumberTest();
+        Assert.assertEquals(5,2,3);
+    }
+
+}
